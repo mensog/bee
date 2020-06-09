98d2035109a71f056f9d125681fb0f7eef245471
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-09 16:15:05 +0300

    Правка вывода продуктов

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -11,11 +11,12 @@
         @foreach ($products as $key => $product)
             <div class="col-lg-3 col-12">
                 <a class="text-decoration-none" href="category/{{ $product->friendly_url_name }}">
-                    <div id="category_{{ $product->id }}" data-sku="{{ $product-> sku }}" class="card product-card mb-3">
+                    <div id="category_{{ $product->id }}" data-sku="{{ $product-> sku }}"
+                         class="card product-card mb-3">
                         <img src="{{ $product->img_url }}" class="card-img-top" alt="{{ $product->name }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $product->name }}</h5>
-                            <p class="card-text">{{ $truncated = Str::of($product->description)->limit(70, ' ...') }}</p>
+                            <p class="card-text">{{ Str::of($product->description)->limit(70, ' ...') }}</p>
 
                             <p class="card-text">{{ $product->price }} руб</p>
                         </div>
