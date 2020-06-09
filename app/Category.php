2228e02af57f711f5c367dfb1e559a2b90306e06
Igor Garifyanov <garifianov.i@gmail.com>
2020-06-09 13:16:15 +0300

    Добавил модель товара, связав её с моделью категории

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Возвращает все продукты из текущей категории
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany('App\Product');
+    }
 }
---
+++ app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * Возвращает категорию текущего товара
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+}
