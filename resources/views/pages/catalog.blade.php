d275360476d0cbb528c1104f9e46b72f387653e7
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-09 12:09:31 +0300

    Убрал php код

--- resources/views/pages/catalog.blade.php
+++ resources/views/pages/catalog.blade.php
@@ -12,27 +12,27 @@
 
 <div class="container">
     <p>
-        Всего категорий: <?= $categories->total(); ?>
+        Всего категорий: {{ $categories->total() }}
     </p>
     <div class="row">
-        <?php foreach ($categories as $key => $category): ?>
+        @foreach ($categories as $key => $category)
         <div class="col-lg-3 col-12">
-            <a href="category/<?= $category->friendly_url_name ?>">
-                <div id="category_<?= $category->id ?>" class="card mb-3">
+            <a href="category/{{ $category->friendly_url_name }}">
+                <div id="category_{{ $category->id }}" class="card mb-3">
                     <div class="card-body">
-                        <h5 class="card-title"><?= $category->name ?></h5>
+                        <h5 class="card-title">{{ $category->name }}</h5>
                     </div>
                 </div>
             </a>
         </div>
-        <?php endforeach; ?>
+        @endforeach
     </div>
     <p>
-        Число страниц: <?= $categories->lastPage(); ?>
+        Число страниц: {{ $categories->lastPage() }}
     </p>
     <div class="row">
         <div class="col">
-            <?php echo $categories->links(); ?>
+            {{ $categories->links() }}
         </div>
     </div>
 </div>
