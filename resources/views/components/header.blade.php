9968a0bcec04f08ca8fb6f4201e91857e6e54f68
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-09 14:55:33 +0300

    Добавил bootstrap-ui

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,8 +6,8 @@
     <title>Главная</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+
 </head>
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
