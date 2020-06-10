6954f633e34fd0eb49008a138c9639a4630b068f
Igor Garifyanov <garifianov.i@gmail.com>
2020-06-10 12:53:23 +0300

    Добавил маршрут добавления товара в корзину

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 Route::get('/catalog', 'CategoryController@index')->name('catalog');
 Route::get('/category/{name}', 'CategoryController@show')->name('category');
 Route::get('/product/{name}', 'ProductController@show')->name('product');
+Route::get('/addtocart', 'CartController@addProduct')->name('add_to_cart');
