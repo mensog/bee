f1e6fea28268f5bdff9d1840c2e0b53a08989f18
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-10 15:14:38 +0300

    Заглушка для пустой корзины

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -6,73 +6,83 @@
             <h1 class="text-center">Корзина</h1>
         </div>
         <div class="mb-10 cart-table">
-            <form class="mb-4" action="#" method="post">
-                <table class="table" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th class="product-thumbnail">&nbsp;</th>
-                        <th class="product-name">Товар</th>
-                        <th class="product-qty">Количество</th>
-                        <th class="product-price">Цена</th>
-                        <th class="product-delete">&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($products as $product)
-                        <tr class="">
-                            <td class="d-none d-md-table-cell">
-                                <a href="{{ route('product', $product->friendly_url_name) }}"><img
-                                        class="img-fluid max-width-100 p-1 border border-color-1"
-                                        src="{{$product->img_url}}" alt="{{$product->description}}"></a>
-                            </td>
-
-                            <td data-title="Product">
-                                <a href="{{ route('category', $product->friendly_url_name) }}" class="text-gray-90">{{$product->name}}</a>
-                            </td>
-
-                            <td data-title="Qty">
-                                <span class="text-gray-90">{{$quantity[$product->id]}}</span>
-                            </td>
-
-                            <td data-title="Price">
-                                <span class="">{{$product->price / 100}} руб</span>
-                            </td>
-
-                            <td data-title="Delete">
-                                <a href="{{route('remove_from_cart', ['product-id' => $product->id] )}}" class="text-gray-90">Удалить</a>
-                            </td>
+            @if(count($products) !== 0)
+                <form class="mb-4" action="#" method="post">
+                    <table class="table" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th class="product-thumbnail">&nbsp;</th>
+                            <th class="product-name">Товар</th>
+                            <th class="product-qty">Количество</th>
+                            <th class="product-price">Цена</th>
+                            <th class="product-delete">&nbsp;</th>
                         </tr>
-                    @endforeach
-                    <tr>
-                        <td colspan="6" class="border-top space-top-2 justify-content-center">
-                            <div class="pt-md-3">
-                                <div class="d-block d-md-flex flex-center-between">
-                                    <div class="mb-3 mb-md-0 w-xl-40">
+                        </thead>
+                        <tbody>
+                        @foreach ($products as $product)
+                            <tr class="">
+                                <td class="d-none d-md-table-cell">
+                                    <a href="{{ route('product', $product->friendly_url_name) }}"><img
+                                            class="img-fluid max-width-100 p-1 border border-color-1"
+                                            src="{{$product->img_url}}" alt="{{$product->description}}"></a>
+                                </td>
 
-                                        <form class="js-focus-state">
-                                            <label class="sr-only" for="subscribeSrEmail">E-mail</label>
-                                            <div class="input-group">
-                                                <input type="text" class="form-control" name="text"
-                                                       id="subscribeSrEmail" placeholder="Введите e-mail"
-                                                       aria-label="email" aria-describedby="subscribeButton"
-                                                       required>
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-block btn-dark px-4" type="button"
-                                                            id="subscribeButton"><i
-                                                            class="fas fa-tags d-md-none"></i><span
-                                                            class="d-none d-md-inline">Оформить заказ</span></button>
+                                <td data-title="Product">
+                                    <a href="{{ route('category', $product->friendly_url_name) }}"
+                                       class="text-gray-90">{{$product->name}}</a>
+                                </td>
+
+                                <td data-title="Qty">
+                                    <span class="text-gray-90">{{$quantity[$product->id]}}</span>
+                                </td>
+
+                                <td data-title="Price">
+                                    <span class="">{{$product->price / 100}} руб</span>
+                                </td>
+
+                                <td data-title="Delete">
+                                    <a href="{{route('remove_from_cart', ['product-id' => $product->id] )}}"
+                                       class="text-gray-90">Удалить</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <td colspan="6" class="border-top space-top-2 justify-content-center">
+                                <div class="pt-md-3">
+                                    <div class="d-block d-md-flex flex-center-between">
+                                        <div class="mb-3 mb-md-0 w-xl-40">
+
+                                            <form class="js-focus-state">
+                                                <label class="sr-only" for="subscribeSrEmail">E-mail</label>
+                                                <div class="input-group">
+                                                    <input type="text" class="form-control" name="text"
+                                                           id="subscribeSrEmail" placeholder="Введите e-mail"
+                                                           aria-label="email" aria-describedby="subscribeButton"
+                                                           required>
+                                                    <div class="input-group-append">
+                                                        <button class="btn btn-block btn-dark px-4" type="button"
+                                                                id="subscribeButton"><i
+                                                                class="fas fa-tags d-md-none"></i><span
+                                                                class="d-none d-md-inline">Оформить заказ</span>
+                                                        </button>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </form>
+                                            </form>
 
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </form>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </form>
+            @else
+                <div>
+                    <p class="text-center font-size-36">Сейчас корзина пуста</p>
+                    <p class="text-center font-size-36">Перейти в <a href="{{route('catalog')}}">каталог</a></p>
+                </div>
+            @endif
         </div>
     </div>
 </main>
