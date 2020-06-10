1e071bccec3802d820d8b2aded1b2e24e6edcdf1
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-10 14:13:17 +0300

    Обновил страницу продукта

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,26 +1,35 @@
 <x-header/>
 
-<body>
-<div class="product">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-12">
-                <img class="product__img" src="{{ $product->img_url }}" alt="{{ $product->name }}">
-            </div>
-            <div class="col-lg-8 col-12">
-                <h1 class="product__title">{{ $product->name }}</h1>
-                <p class="product__breadcrumbs"><a href="{{ route('category', $product->category->friendly_url_name) }}">{{$product->category->name}}/{{ $product->name }}</a></p>
-                <p class="product__description">Артикул: {{ $product-> sku }}</p>
-                <p class="product__description">{{ $product->description }}</p>
-                <p class="product__price">{{ $product->price / 100 }} ₽</p>
-                <p class="product__weight">Вес: {{ $product->weight / 1000 }} кг</p>
-                <form class="product-form" action="" data-id="{{ $product->id }}">
-                    <button class="btn btn-success" type="submit">Добавить в корзину</button>
-                </form>
+<main id="content" role="main">
+    <div class="container mt-3">
+        <div class="mb-xl-14 mb-6">
+            <div class="row">
+                <div class="col-md-5 mb-4 mb-md-0">
+                    <img class="product__img" src="{{ $product->img_url }}" alt="{{ $product->name }}">
+                </div>
+                <div class="col-md-7 mb-md-6 mb-lg-0">
+                    <div class="mb-2">
+                        <div class="border-bottom mb-3 pb-md-1 pb-3">
+                            <a href="{{ route('category', $product->category->friendly_url_name) }}"
+                               class="font-size-12 text-gray-5 mb-2 d-inline-block">{{$product->category->name}}/{{ $product->name }}</a>
+                            <h2 class="font-size-25 text-lh-1dot2">{{ $product->name }}</h2>
+                        </div>
+                        <p>{{ $product->description }}</p>
+                        <p>SKU: {{ $product-> sku }}</p>
+                        <div class="mb-4">
+                            <div class="d-flex align-items-baseline">
+                                <ins class="font-size-36 text-decoration-none">{{ $product->price / 100 }} руб</ins>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <a href="#" class="btn px-5 btn-primary-dark transition-3d-hover"><i
+                                    class="ec ec-add-to-cart mr-2 font-size-20"></i> Добавить в корзину</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</body>
+</main>
 
 <x-footer/>
