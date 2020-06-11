f01d7ab4efefb4e4074ddcdf941bd65ac69f4bd0
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-11 11:13:14 +0300

    Исправление после конфликта гита

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js(['resources/js/app.js', 'resources/js/owl.carousel.js'], 'public/js').version()
-    .sass('resources/sass/app.scss', 'public/css').version()
     .styles(['resources/css/app.css', 'resources/css/animate.css/animate.css', 'resources/css/font-awesome/fontawesome-all.min.css',
         'resources/css/vendor/hs-megamenu/src/hs.megamenu.css', 'resources/css/vendor/ion-rangeslider/css/ion.rangeSlider.css',
         'resources/css/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css', 'resources/css/vendor/fancybox/jquery.fancybox.css',
