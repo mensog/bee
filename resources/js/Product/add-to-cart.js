69907dc23f79c81bc97be32e855cbcaa1c6fa20e
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-22 18:10:39 +0300

    Поправил ajax для добавления в корзину

--- resources/js/Product/add-to-cart.js
+++ resources/js/Product/add-to-cart.js
@@ -1,18 +1,28 @@
-$('.add-to-cart').on('click', function () {
-
-    const id = $(this).data('id');
+$('body').on('click', '.add-to-cart:not(.loading)', function (e) {
+    e.preventDefault()
+    const productId = $(this).data('id');
     const quantity = $(this).data('quantity');
-
+    const action = 'add';
+    const data = {
+        productId,
+        quantity,
+        action
+    }
+    console.log(data)
     $.ajax({
         type: 'POST',
-        url: "",
-        data: {id: id, quantity: quantity},
+        url: "/api/cart",
+        data: JSON.stringify(data),
+        contentType: 'application/json',
         beforeSend: () => {
             $(this).addClass('loading')
         },
         success: data => {
+            if (data['count'] !== '') {
+                $('#cartCounter').html(data['count'])
+            }
             $(this).removeClass('loading')
-            console.log(data.success);
+            console.log(data);
         },
         error: e => {
             console.log(e)
