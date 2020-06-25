4169b8027a6d573532ceb08345065a14a2c54c53
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-25 11:58:44 +0300

    Поправил отступ для инпута, исправил ссылку на товар

--- resources/views/components/cart.blade.php
+++ resources/views/components/cart.blade.php
@@ -5,22 +5,22 @@
             @foreach ($products as $product)
                 <div class="row">
                     <div class="col-lg-2">
-                        <a href="{{ route('product', $product->friendly_url_name) }}"><img
+                        <a href="{{route('product', $product->friendly_url_name)}}"><img
                                 class="img-fluid card-cart__img max-width-100"
                                 src="{{$product->img_url}}" alt="{{$product->description}}"></a>
                     </div>
                     <div class="col-lg-5">
                         <p class="card-cart__title">
-                            <a href="{{ route('category', $product->friendly_url_name) }}"
+                            <a href="{{route('product', $product->friendly_url_name)}}"
                                class="text-gray-90">{{$product->name}}</a>
                         </p>
-                        <p class="card-cart__sku">Артикул: {{ $product-> sku }}</p>
+                        <p class="card-cart__sku">Артикул: {{$product-> sku}}</p>
                     </div>
                     <div class="col-lg-5 card-cart__props">
                         <div class="row">
                             <div class="col-lg-6 text-right col-12 card-cart__price">
                                 <p class="card-cart__price-total">{{$itemsSubTotal[$product->id] / 100}} ₽</p>
-                                <p>{{ $product->price / 100 }} ₽/шт.</p>
+                                <p>{{$product->price / 100}} ₽/шт.</p>
                             </div>
                             <div class="col-lg-2 card-cart__qty">
                                 <input data-quantity="{{$quantity[$product->id]}}"
@@ -44,23 +44,21 @@
         <div class="card-cart__footer">
             <div class="row">
                 <div class="col-lg-7">
-                    <form action="{{ route('checkout') }}" method="post">
-                        <div class="pt-md-3">
-                            <div class="d-block d-md-flex flex-center-between">
-                                <div class="mb-3 mb-md-0 w-xl-80">
-                                    <label class="sr-only" for="subscribeSrEmail">E-mail</label>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" name="email"
-                                               id="subscribeSrEmail" placeholder="Введите e-mail"
-                                               aria-label="email" aria-describedby="subscribeButton"
-                                               required>
-                                        <div class="input-group-append">
-                                            <button class="btn btn-block btn-dark px-4" type="submit"
-                                                    id="subscribeButton"><i
-                                                    class="fas fa-tags d-md-none"></i><span
-                                                    class="d-none d-md-inline">Заказ в 1 клик</span>
-                                            </button>
-                                        </div>
+                    <form action="{{route('checkout')}}" method="post">
+                        <div class="d-block d-md-flex flex-center-between">
+                            <div class="mb-3 mb-md-0 w-xl-80">
+                                <label class="sr-only" for="subscribeSrEmail">E-mail</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="email"
+                                           id="subscribeSrEmail" placeholder="Введите e-mail"
+                                           aria-label="email" aria-describedby="subscribeButton"
+                                           required>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-block btn-dark px-4" type="submit"
+                                                id="subscribeButton"><i
+                                                class="fas fa-tags d-md-none"></i><span
+                                                class="d-none d-md-inline">Заказ в 1 клик</span>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -71,7 +69,7 @@
                     <div class="row">
                         <div class="col-lg-6 col-12 card-cart-total__price">
                             <p class="card-cart-total__title">Итого:</p>
-                            <p>{{ $cartTotal / 100 }} ₽</p>
+                            <p>{{$cartTotal / 100}} ₽</p>
                         </div>
                         <div class="col-lg-6 col-12 card-cart-total__delete">
                             <button type="button" class="btn btn-success">
