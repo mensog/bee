138a7586b33e7aa7412754c0432c86db8a7d6f4a
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-30 13:22:15 +0300

    Обновление проверки формы регистрации

--- resources/js/Registration/registration.js
+++ resources/js/Registration/registration.js
@@ -2,69 +2,160 @@ import is from '../../../node_modules/is_js/is'
 
 let isValid = [];
 
-$('.registration #email').on('keyup', function () {
-    if (is.email($(this).val())) {
-        $(this).removeClass('is-invalid')
-        $(this).addClass('is-valid')
-        if (!isValid.includes($(this).attr('id'))) {
-            isValid.push($(this).attr('id'))
-        }
+$('.registration #email, .registration #name, .registration #surname, .registration #password, .registration #password-confirm').on('keyup', function () {
+    checkRegistrationForm(false, $(this))
+})
+
+$('.registration #personal-data-agreement').on('change', function () {
+    checkRegistrationForm(false, $(this))
+})
+
+const checkRegistrationForm = (load, $this) => {
+    if (load) {
+        $('.registration input').each(function () {
+            switch ($(this).attr('type')) {
+                case 'text':
+                    if ($(this).val().trim() !== '') {
+                        $(this).removeClass('is-invalid')
+                        $(this).addClass('is-valid')
+                        if (!isValid.includes($(this).attr('id'))) {
+                            isValid.push($(this).attr('id'))
+                        }
+                    } else {
+                        $(this).removeClass('is-valid')
+                        if (!load)
+                            $(this).addClass('is-invalid')
+                        let index = isValid.indexOf($(this).attr('id'));
+                        if (index !== -1) isValid.splice(index, 1);
+                    }
+                    break
+                case 'password':
+                    if ($(this).attr('id') === 'password-confirm') {
+                        let password = $('#password').val()
+                        if ($(this).val() === password && $(this).val().length >= 8) {
+                            $(this).removeClass('is-invalid')
+                            $(this).addClass('is-valid')
+                            if (!isValid.includes($(this).attr('id'))) {
+                                isValid.push($(this).attr('id'))
+                            }
+                        } else {
+                            $(this).removeClass('is-valid')
+                            if (!load)
+                                $(this).addClass('is-invalid')
+                            let index = isValid.indexOf($(this).attr('id'));
+                            if (index !== -1) isValid.splice(index, 1);
+                        }
+                    } else {
+                        if ($(this).val().length >= 8) {
+                            $(this).removeClass('is-invalid')
+                            $(this).addClass('is-valid')
+                            if (!isValid.includes($(this).attr('id'))) {
+                                isValid.push($(this).attr('id'))
+                            }
+                        } else {
+                            $(this).removeClass('is-valid')
+                            if (!load)
+                                $(this).addClass('is-invalid')
+                            let index = isValid.indexOf($(this).attr('id'));
+                            if (index !== -1) isValid.splice(index, 1);
+                        }
+                    }
+                    break
+                case 'checkbox':
+                    if ($(this).is(":checked")) {
+                        if (!isValid.includes($(this).attr('id'))) {
+                            isValid.push($(this).attr('id'))
+                        } else {
+                            let index = isValid.indexOf($(this).attr('id'));
+                            if (index !== -1) isValid.splice(index, 1);
+                        }
+                    }
+                    break
+            }
+        })
     } else {
-        $(this).addClass('is-invalid')
-        removeA(isValid, $(this).attr('id'))
-    }
-})
-
-$('.registration #password').on('keyup', function () {
-    if ($(this).val().length >= 8) {
-        $(this).removeClass('is-invalid')
-        $(this).addClass('is-valid')
-        if (!isValid.includes($(this).attr('id'))) {
-            isValid.push($(this).attr('id'))
+        console.log($this.attr('type'))
+        switch ($this.attr('type')) {
+            case 'text':
+                if ($this.val().trim() !== '') {
+                    $this.removeClass('is-invalid')
+                    $this.addClass('is-valid')
+                    if (!isValid.includes($this.attr('id'))) {
+                        isValid.push($this.attr('id'))
+                    }
+                } else {
+                    $this.removeClass('is-valid')
+                    if (!load)
+                        $this.addClass('is-invalid')
+                    let index = isValid.indexOf($this.attr('id'));
+                    if (index !== -1) isValid.splice(index, 1);
+                }
+                break
+            case 'email':
+                if (is.email($this.val())) {
+                    $this.removeClass('is-invalid')
+                    $this.addClass('is-valid')
+                    if (!isValid.includes($this.attr('id'))) {
+                        isValid.push($this.attr('id'))
+                    }
+                } else {
+                    $this.removeClass('is-valid')
+                    if (!load)
+                        $this.addClass('is-invalid')
+                    let index = isValid.indexOf($this.attr('id'));
+                    if (index !== -1) isValid.splice(index, 1);
+                }
+                break
+            case 'password':
+                if ($this.attr('id') === 'password-confirm') {
+                    let password = $('#password').val()
+                    if ($this.val() === password && $this.val().length >= 8) {
+                        $this.removeClass('is-invalid')
+                        $this.addClass('is-valid')
+                        if (!isValid.includes($this.attr('id'))) {
+                            isValid.push($this.attr('id'))
+                        }
+                    } else {
+                        $this.removeClass('is-valid')
+                        if (!load)
+                            $this.addClass('is-invalid')
+                        let index = isValid.indexOf($this.attr('id'));
+                        if (index !== -1) isValid.splice(index, 1);
+                    }
+                } else {
+                    if ($this.val().length >= 8) {
+                        $this.removeClass('is-invalid')
+                        $this.addClass('is-valid')
+                        if (!isValid.includes($this.attr('id'))) {
+                            isValid.push($this.attr('id'))
+                        }
+                    } else {
+                        $this.removeClass('is-valid')
+                        if (!load)
+                            $this.addClass('is-invalid')
+                        let index = isValid.indexOf($this.attr('id'));
+                        if (index !== -1) isValid.splice(index, 1);
+                    }
+                }
+                break
+            case 'checkbox':
+                if ($this.is(":checked")) {
+                    if (!isValid.includes($this.attr('id'))) {
+                        isValid.push($this.attr('id'))
+                    } else {
+                        let index = isValid.indexOf($this.attr('id'));
+                        if (index !== -1) isValid.splice(index, 1);
+                    }
+                }
+                break
         }
-    } else {
-        $(this).addClass('is-invalid')
-        removeA(isValid, $(this).attr('id'))
     }
-})
 
-$('.registration #password-confirm').on('keyup', function () {
-    let password = $('#password').val()
-    if ($(this).val() === password) {
-        $(this).removeClass('is-invalid')
-        $(this).addClass('is-valid')
-        if (!isValid.includes($(this).attr('id'))) {
-            isValid.push($(this).attr('id'))
-        }
-    } else {
-        $(this).addClass('is-invalid')
-        removeA(isValid, $(this).attr('id'))
-    }
-})
-
-$('.registration input').on('change', function () {
-    checkRegistrationForm()
-})
-
-const checkRegistrationForm = () => {
-    let nameVal = $('.registration #name').val().trim()
-    let surNameVal = $('.registration #surname').val().trim()
-    let dataAgree = $('.registration #personal-data-agreement').is(":checked")
-
-    if (isValid.length === 3 && nameVal !== '' && surNameVal !== '' && dataAgree) {
+    if (isValid.length === 6) {
         $('.registration button[type=submit]').prop('disabled', false)
     } else {
         $('.registration button[type=submit]').prop('disabled', true)
     }
 }
 
-const removeA = arr => {
-    let what, a = arguments, L = a.length, ax;
-    while (L > 1 && arr.length) {
-        what = a[--L];
-        while ((ax = arr.indexOf(what)) !== -1) {
-            arr.splice(ax, 1);
-        }
-    }
-    return arr;
-}
+checkRegistrationForm('start')
