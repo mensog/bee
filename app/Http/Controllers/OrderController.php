d9b772553e0bd693341f9afde969b0397493d1bc
Igor Garifyanov <garifianov.i@gmail.com>
2020-07-01 21:11:07 +0300

    Доработал контроллер оформления заказа

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,10 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Cart;
 use App\Order;
+use App\OrderStatus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
+    protected function orderCreateValidator(array $data)
+    {
+        $messages = [
+            'required' => 'Поле :attribute обязательно для заполнения.',
+            'phone.min' => 'Телефон должен состоять из 11 символов (7 XXX XXX XX XX)',
+            'phone.max' => 'Телефон должен состоять из 11 символов (7 XXX XXX XX XX)',
+            'max' => 'Поле :attribute должно содержать не более :max символов',
+        ];
+
+        $names = [
+            'fullName' => 'имя и фамилия',
+            'email' => 'e-mail',
+            'phone' => 'телефон',
+            'address' => 'адрес',
+        ];
+
+        return Validator::make($data, [
+            'fullName' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'min:11', 'max:11'],
+            'address' => ['required', 'string', 'max:255'],
+        ], $messages, $names);
+    }
+
     /**
      * Контроллер создания заказа
      *
@@ -17,8 +44,17 @@ class OrderController extends Controller
     public function create(Request $request)
     {
         $cart = Cart::current();
+        if (count($cart->content) == 0) {
+            return redirect()->route('home');
+        }
+        $this->orderCreateValidator($request->all())->validate();
         $order = new Order();
+        $order->user_id = Auth::user()->id;
+        $order->full_name = $request->input('fullName');
         $order->email = $request->input('email');
+        $order->phone = $request->input('phone');
+        $order->address = $request->input('address');
+        $order->status = OrderStatus::PENDING;
         $order->save();
         $order->fillFromCart($cart);
         $cart->clear();
