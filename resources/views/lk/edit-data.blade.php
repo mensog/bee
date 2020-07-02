4e71b5429fc2becb5aabd1cdf7527039a3179a89
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-07-02 18:09:14 +0300

    Убрал телефон и адрес со страницы изменения личной инфы

--- resources/views/lk/edit-data.blade.php
+++ resources/views/lk/edit-data.blade.php
@@ -4,6 +4,8 @@
     <h2>Изменение личных данных</h2>
     <div class="card-lk">
         <div class="card-lk__body">
+
+            {{ var_dump($user) }}
             <form id="editData" action="{{ route('lk_profile_edit_data') }}">
                 @csrf
                 <div class="form-group row">
@@ -20,24 +22,6 @@
                                placeholder="Введите фамилию"
                                class="form-control" name="surname"
                                value="{{ $user->surname }}">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-md-12">
-                        <label for="phone">Телефон</label>
-                        <input id="phone" type="phone"
-                               placeholder="+7 (000) 000-00-00"
-                               class="form-control" name="phone"
-                               value="{{ $user->phone }}">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-md-12">
-                        <label for="address">Адрес доставки</label>
-                        <input id="address" type="text"
-                               placeholder="Введите адрес доставки"
-                               class="form-control" name="address"
-                               value="{{ $user->address }}">
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary m-auto d-table">
