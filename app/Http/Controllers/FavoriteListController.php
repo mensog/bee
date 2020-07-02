7ff22d6a504add1d964ec1dbc7eb128e9796bbc9
Igor Garifyanov <garifianov.i@gmail.com>
2020-07-02 11:13:02 +0300

    Добавил модель избранного с миграцией и контроллером

---
+++ app/Http/Controllers/FavoriteListController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FavoriteListController extends Controller
+{
+    /**
+     * Обработка апи-запросов к избранному
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     * @throws \Throwable
+     */
+    public function api(Request $request)
+    {
+        $possibleActions = ['add', 'remove'];
+        $action = $request->input('action');
+        $response = response()->json([], 404);
+        if (in_array($action, $possibleActions)) {
+            $productId = (int)$request->input('productId');
+            $favoriteList = app('FavoriteList');
+
+            if ($action === 'add') {
+                $favoriteList->addProduct($productId);
+            } else if ($action === 'remove') {
+                $favoriteList->removeProduct($productId);
+            }
+
+            $response = [
+                'count' => $favoriteList->countTotalQuantity(),
+            ];
+            if ($request->input('fromPage') === 'favorites') {
+                $products = $favoriteList->getProducts();
+                $response['html'] = view('components.favorites', ['products' => $products])->render();
+            }
+            return response()->json($response);
+        }
+        return $response;
+    }
+}
