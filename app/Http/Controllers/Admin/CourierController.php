c7ef4363ab137fbbe0f664a2ec5c4cf2ad9f003d
Igor Garifyanov <garifianov.i@gmail.com>
2020-08-03 14:21:33 +0300

    Добавил бекенд создания и редактирования курьеров

--- app/Http/Controllers/Admin/CourierController.php
+++ app/Http/Controllers/Admin/CourierController.php
@@ -25,4 +25,50 @@ class CourierController extends Controller
         $courier = Courier::with('orders')->where('id', $id)->firstOrFail();
         return view('pages.admin.courier', ['courier' => $courier]);
     }
+
+    protected function courierCreateOrUpdateValidator(array $data)
+    {
+        $messages = [
+            'required' => 'Поле :attribute обязательно для заполнения.',
+            'max' => 'Поле :attribute должно содержать не более :max символов',
+        ];
+
+        $names = [
+            'fullName' => 'ФИО',
+            'phone' => 'телефон',
+            'carSize' => 'размер машины',
+            'commission' => 'условия сотрудничества',
+        ];
+
+        return Validator::make($data, [
+            'fullName' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'carSize' => ['required', 'string', 'max:255'],
+            'commission' => ['required', 'integer', 'max:100'],
+        ], $messages, $names);
+    }
+
+    public function create(Request $request)
+    {
+        $this->courierCreateOrUpdateValidator($request->all())->validate();
+        $courier = new Courier();
+        $courier->full_name = $request->input('fullName');
+        $courier->phone = $request->input('phone');
+        $courier->car_size = $request->input('carSize');
+        $courier->commission = $request->input('commission');
+        $courier->save();
+        return redirect()->route('admin_courier', $courier->id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->courierCreateOrUpdateValidator($request->all())->validate();
+        $courier = Courier::where('id', $id)->findOrFail();
+        $courier->full_name = $request->input('fullName');
+        $courier->phone = $request->input('phone');
+        $courier->car_size = $request->input('carSize');
+        $courier->commission = $request->input('commission');
+        $courier->save();
+        return redirect()->route('admin_courier', $courier->id);
+    }
 }
