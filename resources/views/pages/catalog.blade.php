cb559263aee2199cf1789fe275bd664bd9384a2c
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-08-12 15:36:26 +0300

    Вывод продуктов в новый дизайн, без категорий

--- resources/views/pages/catalog.blade.php
+++ resources/views/pages/catalog.blade.php
@@ -14,7 +14,7 @@
                 <li class="breadcrumb__item"><a class="breadcrumb__link" href="#">Строительство и ремонт</a></li>
             </ul>
             <div class="delivery__box">
-                <img class="delivery__box-img" src="../img/catalog/leroy-merlin-logo.png" alt="">
+                <img class="delivery__box-img" src="/img/catalog/leroy-merlin-logo.png" alt="">
                 <h3 class="delivery__box-title"><span>Доставка из</span>Леруа Мерлен</h3>
             </div>
         </div>
@@ -28,30 +28,31 @@
                     <div class="catalog__aside">
                         <div class="catalog__subtitle">Категории</div>
                         <ul class="catalog__list">
-                            <li class="catalog__list-item"><a class="catalog__list-link" href="#">Строительство и ремонт</a>
+                            <li class="catalog__list-item">
+                                <a class="catalog__list-link" href="#">Строительство и ремонт</a>
                                 <ul class="catalog__sublist">
-                                    <li class="catalog__sublist-item"><a class="catalog__sublist-link" href="#">Расходные материалы</a>
+                                    <li class="catalog__sublist-item">
+                                        <a class="catalog__sublist-link" href="#">Расходные материалы</a>
                                         <ul class="catalog__dropdown">
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Сухие смеси и грунтовка</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Листовые материалы</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Блоки для строительства</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Водосточные и дренажные системы</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Металлопрокат</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Изоляционные материалы</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="catalog__list-item"><a class="catalog__list-link" href="#">Столярные изделия</a>
-                                <ul class="catalog__sublist">
-                                    <li class="catalog__sublist-item"><a class="catalog__sublist-link" href="#">Расходные материалы</a>
-                                        <ul class="catalog__dropdown">
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Сухие смеси и грунтовка</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Листовые материалы</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Блоки для строительства</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Водосточные и дренажные системы</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Металлопрокат</a></li>
-                                            <li class="catalog__dropdown-item"><a class="catalog__dropdown-link" href="#">Изоляционные материалы</a></li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link" href="#">Сухие смеси и грунтовка</a>
+                                            </li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link" href="#">Листовые материалы</a></li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link" href="#">Блоки для строительства</a>
+                                            </li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link"
+                                                   href="#">Водосточные и дренажные системы
+                                                </a>
+                                            </li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link" href="#">Металлопрокат</a>
+                                            </li>
+                                            <li class="catalog__dropdown-item">
+                                                <a class="catalog__dropdown-link" href="#">Изоляционные материалы</a>
+                                            </li>
                                         </ul>
                                     </li>
                                 </ul>
@@ -78,15 +79,33 @@
                             </div>
                         </div>
                         <div class="tabs__content">
-                            <div class="tabs__content-item">
-                                <div class="tabs__content-img">
-                                    <img src="img/catalog/product/img-1.png" alt="">
+                            @foreach ($products as $key => $product)
+                                <div id="product_{{ $product->id }}" class="tabs__content-item">
+                                    <a href="{{ route('product', $product->friendly_url_name) }}">
+                                        <div class="tabs__content-img">
+                                            <img src="{{ $product->img_url }}" alt="{{ $product->name }}">
+                                        </div>
+                                    </a>
+                                    <a href="{{ route('product', $product->friendly_url_name) }}">
+                                        <div class="tabs__content-descr">{{ $product->name }}</div>
+                                        <div class="tabs__content-article">{{ $product->sku }}</div>
+                                        <div class="tabs__content-price">
+                                            <span>{{ $product->price / 100 }} ₽</span>/ за 1 шт
+                                        </div>
+                                    </a>
+                                    @if(isset($cartContent[$product->id]))
+                                        <button data-id="{{ $product->id }}" data-quantity="1"
+                                                class="tabs__content-btn btn">
+                                            В корзине
+                                        </button>
+                                    @else
+                                        <button data-id="{{ $product->id }}" data-quantity="1"
+                                                class="tabs__content-btn btn add-to-cart">
+                                            В корзине
+                                        </button>
+                                    @endif
                                 </div>
-                                <div class="tabs__content-descr">Штукатурка гипсовая Axton 5 <br> кг</div>
-                                <div class="tabs__content-article">81946334</div>
-                                <div class="tabs__content-price"><span>416 ₽</span>/ за 1 шт</div>
-                                <button class="tabs__content-btn btn">В корзину</button>
-                            </div>
+                            @endforeach
                             <div class="tabs__content-item">
                                 <div class="tabs__content-img">
                                     <img src="img/catalog/product/img-2.png" alt="">
@@ -115,113 +134,12 @@
                                 <button class="tabs__content-btn btn">В корзину</button>
                             </div>
                         </div>
+                        {{ $products->links() }}
                     </div>
-                    
                 </div>
             </div>
         </div>
     </div>
-
-    {{-- <div class="container mt-3">
-        <div class="row mb-8">
-            <div class="d-none d-xl-block col-xl-3 col-wd-2gdot5">
-                <div class="mb-6 border border-width-2 border-color-3 borders-radius-6">
-                    <ul id="sidebarNav" class="list-unstyled mb-0 sidebar-navbar view-all">
-                        <li>
-                            <div class="dropdown-title">Категории товаров {{ $categories->count() }}</div>
-                        </li>
-                        @foreach ($categories as $key => $category)
-                            <li id="category_{{ $category->id }}">
-                                <a class="dropdown-toggle dropdown-toggle-collapse"
-                                   href="{{ route('category', $category->friendly_url_name) }}" role="button">
-                                    {{ $category->name }} ({{ $category->products_count }})<span
-                                        class="text-gray-25 font-size-12 font-weight-normal"></span>
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-xl-9 col-wd-9gdot5">
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane mb-8 fade pt-2 show active" id="pills-two-example1" role="tabpanel"
-                         aria-labelledby="pills-two-example1-tab" data-target-group="groups">
-                        <ul class="row list-unstyled products-group no-gutters">
-                            @foreach ($products as $key => $product)
-                                <li id="product_{{ $product->id }}"
-                                    class="col-6 col-md-3 col-wd-2gdot4 product-item {{ $key + 1 % 4 ? 'remove-divider-md-lg remove-divider-xl' : ''}} {{ ($key + 1) % 5  === 0 ? 'remove-divider-wd' : '' }}">
-                                    <div class="product-item__outer h-100">
-                                        <div class="product-item__inner px-xl-4 p-3">
-                                            <div class="product-item__body pb-xl-2">
-                                                <div class="mb-2"><a
-                                                        href="{{ route('product', $product->friendly_url_name) }}"
-                                                        class="font-size-12 text-gray-5">{{ $product->category->name }}</a>
-                                                </div>
-                                                <h5 class="mb-1 product-item__title"><a
-                                                        href="{{ route('product', $product->friendly_url_name) }}"
-                                                        class="text-blue font-weight-bold">{{ $product->name }}</a></h5>
-                                                <div class="mb-2">
-                                                    <a href="{{ route('product', $product->friendly_url_name) }}"
-                                                       class="d-block text-center"><img
-                                                            class="img-fluid" src="{{ $product->img_url }}"
-                                                            alt="{{ $product->name }}"></a>
-                                                </div>
-                                                <p class="font-size-12 p-0 text-gray-110 mb-4">{{ Str::of($product->description)->limit(75, ' ...') }}</p>
-                                                <div class="text-gray-20 mb-2 font-size-12">
-                                                    Артикул: {{ $product-> sku }}</div>
-                                                <div class="flex-center-between mb-1">
-                                                    <div class="prodcut-price">
-                                                        <div class="text-gray-100">{{ $product->price / 100 }} руб</div>
-                                                    </div>
-                                                    <div class="d-none d-xl-block product-add-cart">
-                                                        @if(isset($cartContent[$product->id]))
-                                                            <button data-id="{{ $product->id }}" data-quantity="1"
-                                                                    class="btn-add-cart border-0 btn-primary transition-3d-hover">
-                                                                <i class="ec ec-shopping-bag"></i>
-                                                            </button>
-                                                        @else
-                                                            <button data-id="{{ $product->id }}" data-quantity="1"
-                                                                    class="btn-add-cart border-0 add-to-cart btn-primary transition-3d-hover">
-                                                                <i class="ec ec-add-to-cart"></i>
-                                                            </button>
-                                                        @endif
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="product-item__footer">
-                                                <div class="border-top pt-2 flex-center-between flex-wrap">
-                                                    <a href="#" class="text-gray-6 font-size-13">
-                                                        <i class="ec ec-compare mr-1 font-size-15"></i>
-                                                        Сравнить
-                                                    </a>
-                                                    @if(in_array($product->id, $favoritesListContent))
-                                                        <button data-id="{{ $product->id }}" data-action="remove"
-                                                                class="btn-add-to-favorites add-to-favorites btn btn-link p-0 text-gray-6 font-size-13">
-                                                            <i class="ec heart mr-1 font-size-15"></i>
-                                                        </button>
-                                                    @else
-                                                        <button data-id="{{ $product->id }}" data-action="add"
-                                                                class="btn-add-to-favorites add-to-favorites btn btn-link p-0 text-gray-6 font-size-13">
-                                                            <i class="ec ec-favorites mr-1 font-size-15"></i>
-                                                        </button>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    {{ $products->links() }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
-
-    
 
 </main>
 
