3dd9b088dc9141794e1e17ca3424990b1b1243d2
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-17 11:24:56 +0300

    Сделал страницу с поставщиками

--- resources/views/pages/suppliers.blade.php
+++ resources/views/pages/suppliers.blade.php
@@ -1,7 +1,104 @@
 <x-header />
 
-<main id="content" role="main">
+<main id="content" role="main" class="suppliers">
+   <section class="main-screen">
+      <div class="container">
+          <ul class="breadcrumb">
+              <li class="breadcrumb__item"><a class="breadcrumb__link" href="#">Главная</a></li>
+              <li class="breadcrumb__item">/</li>
+              <li class="breadcrumb__item"><a class="breadcrumb__link" href="#">Поставщикам</a></li>
+          </ul>
+          <h1 class="main-screen__title">Продавайте товары и <br> зарабатывайте вместе с нами</h1>
+          <h2 class="main-screen__subtitle">Дополнительные продажи товара, большие объемы, партнерские условия</h2>
+          <div class="main-screen__benefits">
+              <div class="main-screen__benefits-item">
+                  <div class="main-screen__benefits-title">10 000 <span>товаров</span></div>
+                  <div class="main-screen__benefits-descr">в день покупают клиенты </div>
+              </div>
+              <div class="main-screen__benefits-item">
+                  <div class="main-screen__benefits-title">2 <span>часа</span></div>
+                  <div class="main-screen__benefits-descr">в месяц потребуется <br> вашего времени</div>
+              </div>
+              <div class="main-screen__benefits-item">
+                  <div class="main-screen__benefits-title"><span>до</span> 17%</div>
+                  <div class="main-screen__benefits-descr">комиссии от продажи удерживаем с поставщика</div>
+              </div>
+          </div>
+          <button class="main-screen__btn btn btn-primary">Начать работу</button>
+      </div>
+  </section>
 
+  <section class="expectations">
+      <div class="container">
+          <h2 class="expectations__title">Что ожидаем от Вас?</h2>
+          <div class="row">
+              <div class="col-4">
+                  <div class="expectations__item expectations__item_gray">
+                      <div class="expectations__item-title">Собственный склад</div>
+                      <div class="expectations__item-text">
+                          <span>В пределах МКАД</span>
+                          <span>Удобный подъезд для погрузки</span>
+                          <span>Чем больше машина, тем больше <br> товаров сможете доставить</span>
+                      </div>
+                      <img src="/img/suppliers/stock.png" alt="cars" class="expectations__item-img">
+                  </div>
+              </div>
+              <div class="col-4">
+                 <div class="expectations__item expectations__item_dark-gray">
+                      <div class="expectations__item-title expectations__item-title_white">Упаковка заказов <br> для погрузки</div>
+                      <div class="expectations__item-text expectations__item-text_white">
+                          <span>Вы упаковываете заказ, мы забираем</span>
+                          <span>Время на упаковку - 24 часа</span>
+                      </div>
+                      <img src="/svg/suppliers/pack.svg" alt="dumbbell" class="expectations__item-img">
+                  </div>
+              </div>
+              <div class="col-4">
+                 <div class="expectations__item expectations__item_yellow">
+                      <div class="expectations__item-title">Менеджер для связи</div>
+                      <div class="expectations__item-text">
+                          <span>Доступный с 8:00 до 20:00</span>
+                          <span>Готовый решать организационные <br> вопросы, ответственный и <br> доброжелательный</span>
+                      </div>
+                      <img src="/svg/suppliers/phone.svg" alt="smile" class="expectations__item-img">
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section class="terms">
+     <div class="container">
+        <h2 class="terms__title">Условия и бонусы</h2>
+        <div class="row">
+           <div class="col-6">
+             <div class="terms__item">
+                <div class="terms__item-title">Большой оборот</div>
+                <div class="terms__item-text">У вас хороший ассортимент? Мы его реализуем. <br>
+                  Мы уже продаем 10 000 товаров в день</div>
+             </div>
+           </div>
+           <div class="col-6">
+             <div class="terms__item">
+                <div class="terms__item-title">Без конкурентов</div>
+                <div class="terms__item-text">Ведем нишевой отбор поставщиков. Гарантируем сохранить <br> ваши позиции без конкурентов</div>
+             </div>
+           </div>
+           <div class="col-6">
+            <div class="terms__item">
+               <div class="terms__item-title">Узнаваемость бренда</div>
+               <div class="terms__item-text">Вашу продукцию увидят и купят тысячи клиентов</div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="terms__item">
+               <div class="terms__item-title">Низкая комиссия</div>
+               <div class="terms__item-text">Единая комиссия - всего 17% от стоимости товара</div>
+            </div>
+          </div>
+        </div>
+     </div>
+  </section>
 </main>
 
 <x-footer />
