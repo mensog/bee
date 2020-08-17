82f23a874e80c46707be5df71e11f25c44d3e221
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-17 17:42:20 +0300

    Закоментировал страницу акций

--- resources/views/pages/promotions.blade.php
+++ resources/views/pages/promotions.blade.php
@@ -1,7 +1,52 @@
-<x-header/>
+<x-header />
 
 <main id="content" role="main">
-
+    {{-- <div class="promotions">
+        <div class="delivery">
+            <div class="container">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb__item"><a class="breadcrumb__link" href="#">Главная</a></li>
+                    <li class="breadcrumb__item">/</li>
+                    <li class="breadcrumb__item"><a class="breadcrumb__link" href="#">Акции</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <h2 class="promotions__heading">Акции и скидки</h2>
+            <div class="promotions__shop">
+                <img class="promotions__shop-logo" src="/svg/shop-icons/leroy-mini.svg" alt="">
+                <h3 class="promotions__shop-name">BeeClub</h3>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <a class="promotions__item" href="#">
+                        <img class="promotions__item-img" src="/img/promotions/check.png" alt="">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на всю садовую технику</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item" href="#">
+                        <img class="promotions__item-img" src="/img/promotions/discount.png" alt="">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на кирпичные изделия</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item" href="#">
+                        <img class="promotions__item-img" src="/img/promotions/appstore.png" alt="">
+                        <div class="promotions__item-bonus">+200 баллов</div>
+                        <div class="promotions__item-descr">за установку мобильного приложения</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_empty" href="#">
+                        <div class="promotions__item-bonus">Ещё +20 услуг</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 </main>
 
-<x-footer/>
+<x-footer />
