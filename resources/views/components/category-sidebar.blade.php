01c6392c2f8a37cee4e4981f96df9d0c938789c3
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-08-18 12:01:36 +0300

    Каталог

--- resources/views/components/category-sidebar.blade.php
+++ resources/views/components/category-sidebar.blade.php
@@ -1,13 +1,14 @@
 <ul class="catalog__list">
-        @foreach($categories[''] as $category)
+    @foreach($categories[''] as $category)
         <li class="catalog__list-item">
-                <a class="catalog__list-link" href="{{ route('category', ['name' => $category->friendly_url_name, 'storeSlug' => $currentStore->slug]) }}">{{ $category->name }}</a>
-        @isset($categories[$category->id])
+            <a class="catalog__list-link"
+               href="{{ route('category', ['name' => $category->friendly_url_name, 'storeSlug' => $currentStore->slug]) }}">{{ $category->name }}</a>
+            @isset($categories[$category->id])
                 @php
-                $childCategories = $categories[$category->id];
+                    $childCategories = $categories[$category->id];
                 @endphp
-                <x-category-sidebar-item :categories=$categories :childCategories=$childCategories :store=$store />
+                <x-category-sidebar-item :categories=$categories :childCategories=$childCategories :store=$store/>
             @endisset
         </li>
-        @endforeach
+    @endforeach
 </ul>
