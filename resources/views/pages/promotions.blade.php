1456e8887a76422345eada53b4dad8b7bc336b73
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-18 11:37:30 +0300

    Сделал страницу с акциями

--- resources/views/pages/promotions.blade.php
+++ resources/views/pages/promotions.blade.php
@@ -1,7 +1,7 @@
 <x-header />
 
 <main id="content" role="main">
-    {{-- <div class="promotions">
+    <div class="promotions">
         <div class="delivery">
             <div class="container">
                 <ul class="breadcrumb">
@@ -20,33 +20,104 @@
             <div class="row">
                 <div class="col-3">
                     <a class="promotions__item" href="#">
-                        <img class="promotions__item-img" src="/img/promotions/check.png" alt="">
                         <div class="promotions__item-bonus">10%</div>
                         <div class="promotions__item-descr">на всю садовую технику</div>
                     </a>
                 </div>
                 <div class="col-3">
-                    <a class="promotions__item" href="#">
-                        <img class="promotions__item-img" src="/img/promotions/discount.png" alt="">
+                    <a class="promotions__item  promotions__item_color-black" href="#">
                         <div class="promotions__item-bonus">10%</div>
                         <div class="promotions__item-descr">на кирпичные изделия</div>
                     </a>
                 </div>
                 <div class="col-3">
                     <a class="promotions__item" href="#">
-                        <img class="promotions__item-img" src="/img/promotions/appstore.png" alt="">
                         <div class="promotions__item-bonus">+200 баллов</div>
                         <div class="promotions__item-descr">за установку мобильного приложения</div>
                     </a>
                 </div>
                 <div class="col-3">
-                    <a class="promotions__item promotions__item_empty" href="#">
+                    <a class="promotions__item promotions__item_empty promotions__item_color-black" href="#">
                         <div class="promotions__item-bonus">Ещё +20 услуг</div>
                     </a>
                 </div>
             </div>
+            <div class="promotions__shop">
+                <img class="promotions__shop-logo" src="/svg/shop-icons/leroy-mini.svg" alt="">
+                <div class="promotions__shop-wrap">
+                    <h3 class="promotions__shop-name">Леруа Мерлен</h3>
+                    <a class="promotions__shop-link" href="#">Каталог Леруа Мерлен</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_green-gradient" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на всю садовую технику</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_green" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на кирпичные изделия</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item" href="#">
+                        <div class="promotions__item-bonus">+200 баллов</div>
+                        <div class="promotions__item-descr">за установку мобильного приложения</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_empty promotions__item_color-black" href="#">
+                        <div class="promotions__item-bonus">Ещё +20 услуг</div>
+                    </a>
+                </div>
+            </div>
+            <div class="promotions__shop">
+                <img class="promotions__shop-logo" src="/svg/shop-icons/obi-mini.svg" alt="">
+                <div class="promotions__shop-wrap">
+                    <h3 class="promotions__shop-name">OBI</h3>
+                    <a class="promotions__shop-link" href="#">Каталог OBI</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_orange-gradient" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на всю садовую технику</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_orange" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на кирпичные изделия</div>
+                    </a>
+                </div>
+            </div>
+            <div class="promotions__shop">
+                <img class="promotions__shop-logo" src="/svg/shop-icons/castorama-mini.svg" alt="">
+                <div class="promotions__shop-wrap">
+                    <h3 class="promotions__shop-name">Castorama</h3>
+                    <a class="promotions__shop-link" href="#">Каталог Castorama</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_blue-gradient" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на всю садовую технику</div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a class="promotions__item promotions__item_blue" href="#">
+                        <div class="promotions__item-bonus">10%</div>
+                        <div class="promotions__item-descr">на кирпичные изделия</div>
+                    </a>
+                </div>
+            </div>
         </div>
-    </div> --}}
+    </div>
 </main>
 
 <x-footer />
