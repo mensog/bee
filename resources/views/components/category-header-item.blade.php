078478c1bda0e31f5589ac7f43556559e721d9ca
Igor Garifyanov <garifianov.i@gmail.com>
2020-08-18 15:33:14 +0300

    Добавил класс категориям без дочек

--- resources/views/components/category-header-item.blade.php
+++ resources/views/components/category-header-item.blade.php
@@ -1,12 +1,14 @@
 <ul class="dropdown__menu">
     @foreach($childCategories as $category)
         <li class="dropdown__menu-item">
-            <a class="dropdown__menu-link" href="{{ route('category', ['name' => $category->friendly_url_name, 'storeSlug' => $currentStore->slug]) }}">{{ $category->name }}</a>
             @isset($categories[$category->id])
+                <a class="dropdown__menu-link" href="{{ route('category', ['name' => $category->friendly_url_name, 'storeSlug' => $currentStore->slug]) }}">{{ $category->name }}</a>
                 @php
                     $childCategories = $categories[$category->id];
                 @endphp
                 <x-category-header-item :categories="$categories" :childCategories="$childCategories" :store="$store"/>
+            @else
+                <a class="dropdown__menu-link menu-last" href="{{ route('category', ['name' => $category->friendly_url_name, 'storeSlug' => $currentStore->slug]) }}">{{ $category->name }}</a>
             @endisset
         </li>
     @endforeach
