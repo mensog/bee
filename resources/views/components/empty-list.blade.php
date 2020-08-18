cc4c5c0bbba3a9161bacc745a670a25c6492f1ae
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-18 13:15:27 +0300

    Update empty-list.blade.php

--- resources/views/components/empty-list.blade.php
+++ resources/views/components/empty-list.blade.php
@@ -4,7 +4,7 @@
             <img class="empty__img" src="/svg/components/empty-list/heart.svg" alt="heart">
             <h2 class="empty__heading">В избранном пусто</h2>
             <div class="empty__description">Добавляйте товары в избранное, чтобы не потерять</div>
-            <button class="empty__btn btn btn-primary">Перейти в каталог</button>
+            <a class="empty__btn btn btn-primary" href="{{ route('main') }}">Перейти к покупкам</a>
         </div>
     </div>
 </div>
@@ -15,7 +15,7 @@
             <img class="empty__img" src="/svg/components/empty-list/basket.svg" alt="heart">
             <h2 class="empty__heading">Корзина пока пуста</h2>
             <div class="empty__description">Вернитесь в каталог, чтобы продолжить покупки</div>
-            <button class="empty__btn btn btn-primary">Перейти в каталог</button>
+            <a class="empty__btn btn btn-primary" href="{{ route('main') }}">Перейти к покупкам</a>
         </div>
     </div>
 </div>
@@ -26,7 +26,7 @@
             <img class="empty__img" src="/svg/components/empty-list/order.svg" alt="heart">
             <h2 class="empty__heading">Заказов пока нет</h2>
             <div class="empty__description">Вернитесь в каталог, чтобы сделать первый заказ</div>
-            <button class="empty__btn btn btn-primary">Перейти в каталог</button>
+            <a class="empty__btn btn btn-primary" href="{{ route('main') }}">Перейти к покупкам</a>
         </div>
     </div>
 </div>
