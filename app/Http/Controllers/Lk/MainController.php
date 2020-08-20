bc52d71c179029cd478951499e343a4ff5d2aad5
Igor Garifyanov <garifianov.i@gmail.com>
2020-08-20 13:12:38 +0300

    Вывел в лк заказы с группировкой по магазинам внутри

--- app/Http/Controllers/Lk/MainController.php
+++ app/Http/Controllers/Lk/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Lk;
 
 use App\Http\Controllers\Controller;
+use App\Partner;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,11 +17,25 @@ class MainController extends Controller
     public function orders(Request $request)
     {
         $orders = Auth::user()->orders()->with('items', 'items.product')->orderBy('created_at', 'desc')->paginate(10);
+        $groupedOrders = [];
+        $storeIds = [];
+        foreach ($orders as $order) {
+            $groupedOrder = $order->items->groupBy('product.store.id');
+            $groupedOrders[] = $order->items->groupBy('product.store.id');
+            $storeIds = array_merge($storeIds, array_keys($groupedOrder->toArray()));
+        }
+        $storeIds = array_unique($storeIds);
+        $stores = Partner::whereIn('id', $storeIds)->get()->keyBy('id');
         if ($request->session()->has('createdOrderId')) {
             $createdOrderId = session('createdOrderId');
         } else {
             $createdOrderId = false;
         }
-        return view('lk.orders', ['orders' => $orders, 'createdOrderId' => $createdOrderId]);
+        return view('lk.orders', [
+            'orders' => $orders,
+            'createdOrderId' => $createdOrderId,
+            'groupedOrders' => $groupedOrders,
+            'stores' => $stores,
+            ]);
     }
 }
