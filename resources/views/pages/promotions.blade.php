427af0bb486c6e2067ec9c735b9f3e4c8056ee5d
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-24 14:54:58 +0300

    Переделал страницу акции

--- resources/views/pages/promotions.blade.php
+++ resources/views/pages/promotions.blade.php
@@ -14,10 +14,34 @@
         <div class="container">
             <h2 class="promotions__heading">Акции и скидки</h2>
             <div class="promotions__shop">
-                <img class="promotions__shop-logo" src="/svg/shop-icons/leroy-mini.svg" alt="">
+                <img class="promotions__shop-logo" src="/svg/promotions/beeclub-logo.svg" alt="">
                 <h3 class="promotions__shop-name">BeeClub</h3>
             </div>
+
             <div class="row">
+                <div class="col-6">
+                    <a href="#" class="promotions__item">
+                        <div class="promotions__item-heading">Первая доставка бесплатно</div>
+                        <div class="promotions__item-descr">Действует при заказе от 1 000 ₽</div>
+                        <img class="promotions__item-img" src="/img/promotions/cart.png" alt="">
+                    </a>
+                </div>
+                <div class="col-6">
+                    <a href="#" class="promotions__item">
+                        <div class="promotions__item-heading">200 баллов за регистрацию</div>
+                        <div class="promotions__item-descr">Создайте профиль на BeeClub и получите баллы на личный счет</div>
+                        <img class="promotions__item-img" src="/img/promotions/cart.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="contact">
+                <a href="#" class="contact__improve">
+                    <div class="contact__improve-text">Что можно улучшить на этой странице?</div>
+                </a>
+            </div>
+
+
+            {{-- <div class="row">
                 <div class="col-3">
                     <a class="promotions__item" href="#">
                         <div class="promotions__item-bonus">10%</div>
@@ -116,7 +140,7 @@
                         <div class="promotions__item-descr">на кирпичные изделия</div>
                     </a>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </main>
