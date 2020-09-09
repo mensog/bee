b2a8613838f6fe916a0216dc0293d2cafc1b0dc1
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-09 12:56:39 +0300

    Закоментировал старый хедер, сделал новый

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,7 +15,7 @@
 
 <body>
 
-<header id="header" class="header">
+<!-- <header id="header" class="header">
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <a href="{{ route('main') }}" class="navbar-brand">
@@ -139,4 +139,125 @@
             </div>
         </div>
     </div>
-</header>
+</header> -->
+
+
+
+<header class="header">
+      <div class="container">
+         <div class="header-wrap">
+            <div class="header-left">
+               <a href="{{ route('main') }}" class="navbar-brand">
+                  <img src="/svg/main/BeeClub-logo-big.svg" alt="BeeClub-logo">
+               </a>
+            </div>
+            <div class="header-right">
+               <nav class="navbar navbar-expand-lg">
+                  <div class="collapse navbar-collapse">
+                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                           <a href="{{ route('promotions') }}" class="nav-link">
+                              Акции
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="{{ route('about') }}" class="nav-link">
+                              О сервисе
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="" class="nav-link">
+                              Доставка
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="{{ route('couriers') }}" class="nav-link">
+                              Курьерам
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="{{ route('suppliers') }}" class="nav-link">
+                              Поставщикам
+                           </a>
+                        </li>
+                     </ul>
+                     <div class="menu-right">
+                        <a class="position" href="#"><img src="/svg/main/position.svg" alt="">Москва</a>
+                        <a class="login" href="{{ route('lk') }}">Войти в профиль</a>
+                     </div>
+                  </div>
+               </nav>
+               <div class="header-inner">
+                  <div class="dropdown">
+                     <a class="catalog-btn btn btn-primary dropdown-toggle {{ (isset($storeCatalog) && $storeCatalog) ? '' : 'dropdown-empty' }}" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="/svg/main/catalog.svg" alt="">Каталог
+                     </a>
+                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        @isset($storeCatalog)
+                        <x-category-header :categories="$storeCatalog" :store="$currentStore" />
+                        @endisset
+                     </div>
+                  </div>
+                  <div class="dropdown dropdown-stores">
+                     <a class="nav-link dropdown-toggle dropdown-btn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div>
+                           @if($currentStore->image_path ?? '')
+                           <span class="dropdown-stores__img">
+                              <img src="{{ $currentStore->image_path ?? '' }}" alt="">
+                           </span>
+                           @endif
+                           <span>{{ $currentStore->company_name ?? 'Выберите магазин' }}</span>
+                        </div>
+                        <img src="/svg/main/accordion-arrow.svg" alt="">
+                     </a>
+                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <ul class="dropdown__mainmenu">
+                           @foreach($headerAllStores as $store)
+                           <li class="dropdown__mainmenu-item menu-last">
+                              <a class="dropdown__mainmenu-link" href="{{ route('store_main', $store->slug) }}">
+                                 <span class="dropdown-stores__img">
+                                    <img src="{{ $store->image_path ?? '' }}" alt="">
+                                 </span>
+                                 {{ $store->company_name }}
+                              </a>
+                           </li>
+                           @endforeach
+                        </ul>
+                     </div>
+                  </div>
+                  <form action="" class="form-inline">
+                     <div class="input-group">
+                        <input placeholder="Хочу найти нужный товар" type="text" class="form-control">
+                        <div class="input-group-append">
+                           <button class="btn btn-primary btn-search"><img src="/svg/main/search.svg" alt=""></button>
+                        </div>
+                     </div>
+                  </form>
+                  <div class="header-icons">
+                     <a href="">
+                        <img src="/svg/main/order.svg" alt="">
+                     </a>
+                     <a href="{{ route('favorites') }}">
+                        <span id="favoritesCounter" class="{{ $headerFavoritesCount ? 'd-block' : 'd-none' }}">
+                           {{ $headerFavoritesCount ?: '' }}
+                        </span>
+                        <img src="/svg/main/favorite.svg" alt="">
+                     </a>
+
+                     <div class="header-cart">
+                        <span id="cartTotal" class="header-cart__price">
+                           {{ number_format($headerCartTotal / 100, 0, ',', ' ') }} ₽
+                        </span>
+                        <a href="{{ route('cart') }}">
+                           <span id="cartCounter" class="{{ $headerCartCount ? 'd-block' : 'd-none' }}">
+                              {{ $headerCartCount ?: '' }}
+                           </span>
+                           <img src="/svg/main/cart.svg" alt="">
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
