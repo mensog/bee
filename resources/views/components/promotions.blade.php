0d605f40e495c4f41e05c6a703b4ba7aecfdcb34
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-10 14:51:44 +0300

    Поправил

--- resources/views/components/promotions.blade.php
+++ resources/views/components/promotions.blade.php
@@ -1,13 +1,20 @@
 <div class="main-sales promotions">
    <div class="container">
-      <h2 class="promotions__heading">Акции и скидки</h2>
-      <div class="promotions__shop">
-         <img class="promotions__shop-logo" src="/svg/promotions/beeclub-logo.svg" alt="">
-         <h3 class="promotions__shop-name">BeeClub</h3>
+      <div class="promotions__header">
+         <h2 class="promotions__heading">Акции и спецпредложения</h2>
+         <a href="#">
+            Смотреть все акции
+            <img src="/svg/main/arrow-right.svg" alt="">
+         </a>
       </div>
 
+      <!-- <div class="promotions__shop">
+         <img class="promotions__shop-logo" src="/svg/promotions/beeclub-logo.svg" alt="">
+         <h3 class="promotions__shop-name">BeeClub</h3>
+      </div> -->
+
       <div class="row">
-         <div class="col-6">
+         <div class="col-md-6">
             <a href="#" class="promotions__item">
                <div class="promotions__item-heading">
                   Первая доставка бесплатно
@@ -18,7 +25,7 @@
                <img class="promotions__item-img" src="/img/promotions/cart.png" alt="">
             </a>
          </div>
-         <div class="col-6">
+         <div class="col-md-6">
             <a href="#" class="promotions__item">
                <div class="promotions__item-heading">
                   200 баллов за регистрацию
--- resources/views/pages/promotions.blade.php
+++ resources/views/pages/promotions.blade.php
@@ -19,14 +19,14 @@
             </div>
 
             <div class="row">
-                <div class="col-6">
+                <div class="col-md-6">
                     <a href="#" class="promotions__item">
                         <div class="promotions__item-heading">Первая доставка бесплатно</div>
                         <div class="promotions__item-descr">Действует при заказе от 1 000 ₽</div>
                         <img class="promotions__item-img" src="/img/promotions/cart.png" alt="">
                     </a>
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                     <a href="#" class="promotions__item">
                         <div class="promotions__item-heading">200 баллов за регистрацию</div>
                         <div class="promotions__item-descr">Создайте профиль на BeeClub и получите баллы на личный счет</div>
