e594a2ce700d47fefd04d03fe147b3f229629a33
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-09-16 11:31:07 +0300

    Добавил фавикон

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <link rel="stylesheet" type="text/css" href="{{ mix('css/admin/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('css/admin/app-sass.css') }}">
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
 
    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
    <link rel="stylesheet" type="text/css" href="{{ mix('css/app-sass.css') }}">
