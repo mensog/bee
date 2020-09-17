2783a1037a681033a85c212c87efa8a5b1685c63
skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-17 11:52:38 +0300

    Сделал уведомления в лк

--- resources/views/components/empty-list.blade.php
+++ resources/views/components/empty-list.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="empty">
             <div class="empty__inner">
-                <img class="empty__img" src="/svg/components/empty-list/basket.svg" alt="heart">
+                <img class="empty__img" src="/svg/components/empty-list/basket.svg" alt="basket">
                 <h2 class="empty__heading">Корзина пока пуста</h2>
                 <div class="empty__description">Вернитесь в каталог, чтобы продолжить покупки</div>
                 <a class="empty__btn btn btn-primary" href="{{ route('main') }}">Перейти к покупкам</a>
@@ -28,7 +28,7 @@
     <div class="container">
         <div class="empty">
             <div class="empty__inner">
-                <img class="empty__img" src="/svg/components/empty-list/order.svg" alt="heart">
+                <img class="empty__img" src="/svg/components/empty-list/order.svg" alt="order">
                 <h2 class="empty__heading">Заказов пока нет</h2>
                 <div class="empty__description">Вернитесь в каталог, чтобы сделать первый заказ</div>
                 <a class="empty__btn btn btn-primary" href="{{ route('main') }}">Перейти к покупкам</a>
@@ -36,3 +36,14 @@
         </div>
     </div>
 @endif
+
+
+<div class="container">
+    <div class="empty">
+        <div class="empty__inner">
+            <img class="empty__img" src="/svg/components/empty-list/notification.svg" alt="notification">
+            <h2 class="empty__heading">Уведомлений нет</h2>
+            <div class="empty__description">У вас пока нет ни одного уведомления.</div>
+        </div>
+    </div>
+</div>
--- resources/views/lk/notification.blade.php
+++ resources/views/lk/notification.blade.php
@@ -3,7 +3,39 @@
 @section('content')
     <div class="card-lk">
         <div class="card-lk__header">
-            <h3 class="card-lk__title">Уведомления</h3>
+            <h3 class="card-lk__title">Уведомления (5)</h3>
+        </div>
+        <div class="card-lk__body card-lk-notifications">
+            <div class="card-lk-notification">
+                <div class="card-lk-notification__header">
+                    <div class="card-lk-notification__title">Заказ у курьера</div>
+                    <div class="card-lk-notification__date">27.07.2020</div>
+                </div>
+                <div class="card-lk-notification__body">
+                    <div class="card-lk-notification__order">Заказ №4548912</div>
+                    <div class="card-lk-notification__descr">доставим сегодня с 09:00 до 14:00</div>
+                </div>
+            </div>
+            <div class="card-lk-notification">
+                <div class="card-lk-notification__header">
+                    <div class="card-lk-notification__title">Заказ у курьера</div>
+                    <div class="card-lk-notification__date">27.07.2020</div>
+                </div>
+                <div class="card-lk-notification__body">
+                    <div class="card-lk-notification__order">Заказ №4548912</div>
+                    <div class="card-lk-notification__descr">доставим сегодня с 09:00 до 14:00</div>
+                </div>
+            </div>
+            <div class="card-lk-notification">
+                <div class="card-lk-notification__header">
+                    <div class="card-lk-notification__title">Заказ у курьера</div>
+                    <div class="card-lk-notification__date">27.07.2020</div>
+                </div>
+                <div class="card-lk-notification__body">
+                    <div class="card-lk-notification__order">Заказ №4548912</div>
+                    <div class="card-lk-notification__descr">доставим сегодня с 09:00 до 14:00</div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
