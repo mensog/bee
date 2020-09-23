786f700bfded997f0e3f4023778010143c677cb7
Igor Garifyanov <garifianov.i@gmail.com>
2020-09-23 12:07:42 +0300

    Добавил data-атрибуты с чпу категории в ссылках сайдбара категорий

--- resources/views/components/category-sidebar-item.blade.php
+++ resources/views/components/category-sidebar-item.blade.php
@@ -2,7 +2,7 @@
     @foreach($childCategories as $category)
         @isset($categories[$category->id])
             <li class="catalog__sublist-item{{ in_array($category->friendly_url_name, $activeCategorySlugs) ? ' active' : '' }}">
-            <a class="catalog__sublist-link" href="{{ route($routeName, array_merge(['name' => $category->friendly_url_name] + request()->query())) }}">{{ $category->name }}</a>
+            <a class="catalog__sublist-link" href="{{ route($routeName, array_merge(['name' => $category->friendly_url_name] + request()->query())) }}" data-category-name="{{ $category->friendly_url_name }}">{{ $category->name }}</a>
             @php
                 $childCategories = $categories[$category->id];
             @endphp
@@ -10,7 +10,7 @@
             :routeName="$routeName"/>
         @else
             <li class="catalog__sublist-item menu-last{{ in_array($category->friendly_url_name, $activeCategorySlugs) ? ' active' : '' }}">
-            <a class="catalog__sublist-link" href="{{ route($routeName, ['name' => $category->friendly_url_name] + request()->query()) }}">{{ $category->name }}</a>
+            <a class="catalog__sublist-link" href="{{ route($routeName, ['name' => $category->friendly_url_name] + request()->query()) }}" data-category-name="{{ $category->friendly_url_name }}">{{ $category->name }}</a>
         @endisset
     </li>
     @endforeach
