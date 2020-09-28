ea5b08609cac16681873f1a7c9cd39b1b715bf5b
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-28 14:35:36 +0300

    Правки

--- resources/views/components/promotions.blade.php
+++ resources/views/components/promotions.blade.php
@@ -3,31 +3,28 @@
       <div class="promotions__header">
          <h2 class="promotions__heading">Акции и спецпредложения</h2>
          <a href="#">
-            Смотреть все акции
-            <img src="/svg/main/arrow-right.svg" alt="">
-         </a>
       </div>
 
       <div class="row">
          <div class="col-md-6">
-            <a href="#" class="promotions__item promotions__item_right">
+            <div class="promotions__item promotions__item_right">
                <div class="promotions__item-heading">
                   Первая доставка бесплатно
                </div>
                <div class="promotions__item-descr">
                   Действует при заказе от 1 000 ₽
                </div>
-            </a>
+            </div>
          </div>
          <div class="col-md-6">
-            <a href="#" class="promotions__item">
+            <div class="promotions__item">
                <div class="promotions__item-heading">
                   200 баллов за регистрацию
                </div>
                <div class="promotions__item-descr">
                   Создайте профиль на BeeClub и получите баллы на личный счет
                </div>
-            </a>
+            </div>
          </div>
       </div>
    </div>
